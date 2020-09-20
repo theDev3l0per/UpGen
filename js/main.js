46931eaffcd6f912add28bd09297cadee851bf93
theDev3l0per <69645887+theDev3l0per@users.noreply.github.com>
2020-09-19 18:31:16 -0600

    commit

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ var saveloop = setInterval(save,10000)
 
 function loop() {
     if (app.player.gens == 1) {
-        app.player.points = new Decimal((app.player.points.add(app.player.value.div(20))).toFixed(1))
+        app.player.points = new Decimal((app.player.points.add(app.player.value.div(20))))
     }
 }
 
