a4ec2c5d6f8f66c82e4ff94826295e9fa23d1331
theDev3l0per <69645887+theDev3l0per@users.noreply.github.com>
2020-09-19 18:12:08 -0600

    commit

--- js/main.js
+++ js/main.js
@@ -4,7 +4,8 @@ const app = new Vue({
         player: {
             tab:"generator",
             points: new Decimal(10),
-            gens:0
+            gens:0,
+            value: 1
         }
     }
 })
@@ -12,7 +13,9 @@ var gameloop = setInterval(loop,50)
 var saveloop = setInterval(save,10000)
 
 function loop() {
-
+    if (app.player.gens == 1) {
+        
+    }
 }
 
 function resetgame() {
